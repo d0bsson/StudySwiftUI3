b1aa710fbd82cfe8cb27fd8ebffece719ebda42c
David <ber.dev@yandex.ru>
2021-06-09 13:41:45 +0300

    Initial Commit

---
+++ StudySwiftUI3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudySwiftUI3
+//
+//  Created by Дэвид Бердников on 09.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudySwiftUI3/StudySwiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUI3App.swift
+//  StudySwiftUI3
+//
+//  Created by Дэвид Бердников on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
